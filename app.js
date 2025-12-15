94712b46337742662eedcb29124cf0149078da75
ikikikk360-design <ikikikk360@gmail.com>
2025-12-16 01:31:33 +0500

    app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("RuDiscord запущен");
+window.addEventListener("DOMContentLoaded", () => {
+  const splash = document.getElementById("splash-screen");
+  const login = document.getElementById("login-screen");
+
+  // имитация загрузки
+  setTimeout(() => {
+    splash.classList.add("hidden");
+    login.classList.remove("hidden");
+  }, 1500);
+});
